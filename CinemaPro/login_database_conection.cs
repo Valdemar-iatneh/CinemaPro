e75719eebe28748688847798a19e1caee56b4563
Waldemar_SSS <avvakumov112003@gmail.com>
2021-11-02 04:17:43 +0300

    Add project files.

---
+++ CinemaPro/login_database_conection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaPro
+{
+    class login_database_conection
+    {
+        public static LoginEntities1 connection = new LoginEntities1();
+    }
+}
---
+++ CinemaPro/page_authorization.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaPro
+{
+    /// <summary>
+    /// Interaction logic for page_authorization.xaml
+    /// </summary>
+    public partial class page_authorization : Page
+    {
+        public static ObservableCollection<login_data> users { get; set; }
+       
+        public page_authorization()
+        {
+            InitializeComponent();
+               
+        }
+
+        private void authoriz_click_event(object sender, RoutedEventArgs e)
+        {
+            users = new ObservableCollection<login_data>(login_database_conection.connection.login_data.ToList());
+            try
+            {
+                var entry = users.Where(a => a.login == text_login.Text && a.password == text_password.Password && a.access_level == access_level.SelectedItem.ToString()).FirstOrDefault();
+                switch (access_level.SelectedItem)
+                {
+                    case "User":
+                        if (entry != null)
+                        {
+                            MessageBox.Show($"{entry.name} {entry.access_level}");
+                            UserWindow userWindow = new UserWindow();
+                            MainWindow mainWindow = new MainWindow();
+                            mainWindow.Close();
+                            userWindow.Show();
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Login or password incorrect", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        break;
+                    case "Manager":
+                        if (entry != null)
+                        {
+                            MessageBox.Show($"{entry.name} {entry.access_level}");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Login or password incorrect", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        break;
+                    case "Admin":
+                        if (entry != null)
+                        {
+                            NavigationService.Navigate(new admin_space());
+                        }
+                        else
+                        {
+                            MessageBox.Show("Login or password incorrect", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        break;
+                    default:
+                        MessageBox.Show($"Не выбрано");
+                        break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Не выбрано");
+            }
+        }
+    }
+}
